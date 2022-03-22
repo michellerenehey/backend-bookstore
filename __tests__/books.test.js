6012ab131e130132e56a370f01671c610a0b6f2b
Kevin Do <kevind0501@gmail.com>
2022-03-22 14:48:01 -0700

    model controller test for find all books

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -18,4 +18,36 @@ describe('bookstore routes', () => {
 
     expect(res.body).toEqual({ book_id: expect.any(String), ...expected });
   });
+
+  it('gets list of books', async () => {
+    const expected = [
+      {
+        book_id: '1',
+        title: 'Data Structures and Algorithms',
+        publisher_id: '2',
+        released: 2018,
+      },
+      {
+        book_id: '2',
+        title: 'How to Graduate Alchemy',
+        publisher_id: '1',
+        released: 2018,
+      },
+      {
+        book_id: '3',
+        title: 'What is a Div',
+        publisher_id: '3',
+        released: 2022,
+      },
+      {
+        book_id: '4',
+        title: 'To Div or Not to Div',
+        publisher_id: '3',
+        released: 2021,
+      },
+    ];
+
+    const res = await request(app).get('/api/v1/books');
+    expect(res.body).toEqual(expected);
+  });
 });
