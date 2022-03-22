2c9191dd83f41af1c09f425ffe78e7d823d3ba3c
Brett Seifried <seifriedbrett@gmail.com>
2022-03-22 12:12:48 -0700

    passing author findAll

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -21,4 +21,14 @@ describe('bookstore routes', () => {
     const res = await request(app).post('/api/v1/authors').send(expected);
     expect(res.body).toEqual({ author_id: expect.any(String), ...expected });
   });
+
+  it('gets a list of authors', async () => {
+    const expected = [
+      { author_id: '1', name: 'Ernest Hemingway' },
+      { author_id: '2', name: 'Margaret Atwood' },
+      { author_id: '3', name: 'Julie Nisbit' },
+    ];
+    const res = await request(app).get('/api/v1/authors');
+    expect(res.body).toEqual(expected);
+  });
 });
